1c55e3f8888b4248cdab087edc99333f34d3a700
Yipeechen <yipeechen0418@gmail.com>
2018-08-26 15:58:00 +0800

    npm install css-loader style-loader file-loader

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // Global app controller
 import script from './script';
+import '../css/style.css';
 
 console.log(`success import ${script} from another module`);
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ['babel-polyfill','./js/index.js'],
+  entry: ["babel-polyfill", "./js/index.js"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js"
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: "./dist"
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './index.html'
+      filename: "index.html",
+      template: "./index.html"
     })
   ],
   module: {
@@ -22,7 +22,17 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_module/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/, // 針對所有.css 的檔案作預處理
+        use: ["style-loader", "css-loader"] // 順序有差異，css先 style後執行
+      },
+      {
+        test: /\.jpg$/,
+        use: {
+          loader: "file-loader"
         }
       }
     ]

6bdb55a163f7554dd594b4c60930b9863f2fdec6
Yipeechen <yipeechen0418@gmail.com>
2018-08-26 11:33:30 +0800

    npm install babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './js/index.js',
+  entry: ['babel-polyfill','./js/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js'

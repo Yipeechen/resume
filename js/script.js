546a38164bcb7b299f6dedc96a3ea843a77dfc41
Yipeechen <yipeechen0418@gmail.com>
2018-09-04 11:30:44 +0800

    add slide message animation

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ $('.js--scroll-to-works').click(function () {
     scrollTop: $('.section-works').offset().top
   }, 1000);
 });
+
+// SlideMessage
+var $window = $(window);
+var $slideMes = $('#slide-message');
+var endZone = $('.section-contact').offset().top - $window.height() - 100;
+
+$window.on('scroll', function() {
+  if (endZone < $window.scrollTop() ) {   // fadeIn
+    $slideMes.animate({
+      'right': '0px'
+    }, 500);
+  } else {
+    $slideMes.stop(true).animate({  // fadeOut
+      'right': '-25rem'
+    }, 250);
+  }
+});

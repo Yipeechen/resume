365bd4e76fb0aeea8c8826e10e7cd9b05860116a
Yipeechen <yipeechen0418@gmail.com>
2020-10-13 20:24:25 +0800

    feat: create resume page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
-// import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import Resume from '@src/features/Resume';
+
+const routes = [
+  {
+    path: '/',
+    exact: true,
+    component: Resume,
+  },
+];
 
 const App = () => (
-  <h1>Hello React</h1>
+  <BrowserRouter>
+    <Switch>
+      {routes.map((route, i) => (
+        <Route key={i} path={route.path} exact={route.exact} component={route.component} />
+      ))}
+      <Route exact path="*" render={() => <Redirect to="/" />} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;

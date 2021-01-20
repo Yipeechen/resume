f7cc18760bb23e9f57d363d5d14e57634329cd81
Yipeechen <yipeechen0418@gmail.com>
2021-01-20 16:38:07 +0800

    build: add react-helmet to control title and meta data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { Helmet } from 'react-helmet';
 
 import theme from '@src/styles/theme';
 import mediaQueries from './styles/mediaQueries';
@@ -15,11 +16,17 @@ const routes = [
     path: '/resume',
     exact: true,
     component: Resume,
+    pageMeta: {
+      title: 'About Yipee | Resume',
+    },
   },
   {
     path: '/works/yt',
     exact: true,
     component: Yt,
+    pageMeta: {
+      title: 'About Yipee | Work',
+    },
   },
 ];
 
@@ -31,9 +38,23 @@ const App = () => (
       <Suspense fallback={<PageSpinner size="50vh" style={{ margin: '25vh auto' }} />}>
         <Switch>
           {routes.map((route, i) => (
-            <Route key={i} path={route.path} exact={route.exact} component={route.component} />
+            <Route
+              key={i}
+              path={route.path}
+              exact={route.exact}
+              render={routeProps => (
+                <>
+                  <Helmet>
+                    <title>{route.pageMeta.title}</title>
+                  </Helmet>
+                  <route.component {...routeProps} />
+                </>
+              )} />
           ))}
-          <Route exact path="*" render={() => <Redirect to="/resume" />} />
+          <Route
+            exact
+            path="*"
+            render={() => <Redirect to="/resume" />} />
         </Switch>
       </Suspense>
     </BrowserRouter>
